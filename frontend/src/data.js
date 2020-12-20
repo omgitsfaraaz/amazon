7db2f71283e3c1db8072cacb164399e4e45b96b2
unknown <mohsinsait23@gmail.com>
2020-12-20 17:41:29 +0530

    added the products dynamically

---
+++ frontend/src/data.js
@@ -0,0 +1,40 @@
+export default {
+    products: [
+        {
+            name: 'Slim shirt',
+            category: 'Shirts',
+            image: '/images/d1.jpg',
+            price: 60,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10
+        },
+        {
+            name: 'Fit shirt',
+            category: 'Shirts',
+            image: '/images/d1.jpg',
+            price: 50,
+            brand: 'Nike',
+            rating: 4.6,
+            numReviews: 9
+        },
+        {
+            name: 'Slim pant',
+            category: 'Pants',
+            image: '/images/d1.jpg',
+            price: 70,
+            brand: 'Nike',
+            rating: 4.2,
+            numReviews: 5
+        },
+        {
+            name: 'Slim pant',
+            category: 'Pants',
+            image: '/images/d1.jpg',
+            price: 70,
+            brand: 'Nike',
+            rating: 4.2,
+            numReviews: 5
+        },
+    ]
+}
