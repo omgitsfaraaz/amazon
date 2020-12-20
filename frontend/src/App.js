c0b598609680ceeb3eff263fd82f6bea62fdaf1f
unknown <mohsinsait23@gmail.com>
2020-12-20 19:03:03 +0530

    added route paths for homescreen and productscreen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import data from './data';
 import './App.css';
+import { BrowserRouter, Route } from 'react-router-dom';
+import HomeScreen from './Screens/HomeScreen';
+import ProductScreen from './Screens/ProductScreen';
 
 function App() {
   const openMenu = () => {
@@ -11,55 +14,59 @@ function App() {
     document.querySelector(".sidebar").classList.remove("open");
   }
   return (
-    <div className="grid-container">
-      <header className="header">
-        <div className="brand">
-          <button onClick={openMenu}>&#9776;</button>
-          <a href="index.html">Amazon</a>
-        </div>
-        <div className="header-links">
-          <a href="cart.html">Cart</a>
-          <a href="signin.html">Sign In</a>
-        </div>
-        </header>
-        <aside className="sidebar">
-          <h3>Shopping Categories</h3>
-          <button className="sidebar-close-button" onClick={closeMenu}>x</button>
-          <ul>
-            <li>
-              <a href="index.html">Pants</a>
-            </li>
-            <li>
-              <a href="index.html">Shirts</a>
-            </li>
-            </ul>
-        </aside>
-        <main className="main">
-          <div className="content">
-            <ul className="products">
-              {
-                data.products.map(product =>
-                  <li>
-                    <div className="product">
-                      <img className="product-image" src={product.image} alt="product"/>
-                      <div className="product-name">
-                        <a href="product.html">{product.name}</a>
-                      </div>
-                      <div className="product-brand">{product.brand}</div>
-                      <div className="product-price">${product.price}</div>
-                      <div className="product-rating">{product.rating} stars ({product.numReviews})</div>
-                    </div>
-                  </li> 
-                )
-              }
-              
-            </ul>
+    <BrowserRouter>
+      <div className="grid-container">
+        <header className="header">
+          <div className="brand">
+            <button onClick={openMenu}>&#9776;</button>
+            <a href="index.html">Amazon</a>
           </div>
-        </main>
-        <footer className="footer">
-          All Rights reserved.
-        </footer>
-    </div>
+          <div className="header-links">
+            <a href="cart.html">Cart</a>
+            <a href="signin.html">Sign In</a>
+          </div>
+          </header>
+          <aside className="sidebar">
+            <h3>Shopping Categories</h3>
+            <button className="sidebar-close-button" onClick={closeMenu}>x</button>
+            <ul>
+              <li>
+                <a href="index.html">Pants</a>
+              </li>
+              <li>
+                <a href="index.html">Shirts</a>
+              </li>
+              </ul>
+          </aside>
+          <main className="main">
+            <div className="content">
+              <Route path='/product/:id' component={ProductScreen} />
+              <Route path='/' exact={true} component={HomeScreen} />
+              <ul className="products">
+                {
+                  data.products.map(product =>
+                    <li>
+                      <div className="product">
+                        <img className="product-image" src={product.image} alt="product"/>
+                        <div className="product-name">
+                          <a href="product.html">{product.name}</a>
+                        </div>
+                        <div className="product-brand">{product.brand}</div>
+                        <div className="product-price">${product.price}</div>
+                        <div className="product-rating">{product.rating} stars ({product.numReviews})</div>
+                      </div>
+                    </li> 
+                  )
+                }
+                
+              </ul>
+            </div>
+          </main>
+          <footer className="footer">
+            All Rights reserved.
+          </footer>
+      </div>
+    </BrowserRouter>
   );
 }
 
