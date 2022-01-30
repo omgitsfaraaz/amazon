2a14b45d152ad8565440c53b595dcbf18d99f610
mohsin16 <mohsin@theretailinsights.com>
2022-01-30 12:45:46 +0530

    added product details

--- frontend/src/Screens/ProductScreen.js
+++ frontend/src/Screens/ProductScreen.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import data from '../data';
 
 function ProductScreen(props) {
@@ -6,7 +7,33 @@ function ProductScreen(props) {
     const product = data.products.find(x => x._id == props.match.params.id);
     return(
         <div>
-            <h1>{product.name}</h1>
+            <div>
+                <Link to='/'>Back to result</Link>
+            </div>
+            <div className="details">
+                <div className="details-image">
+                    <img src={product.image} alt="product"></img>
+                </div>
+                <div className="details-info">
+                    <ul>
+                        <li>
+                            <h4>{product.name}</h4>
+                        </li>
+                        <li>
+                            {product.rating} Stars ({product.numReviews} Reviews)
+                        </li>
+                        <li>
+                            <b>{product.price}</b>
+                        </li>
+                        <li>
+                            Description:
+                            <div>
+                                {product.description}
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </div>
     )
 }
